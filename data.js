a75fcbff18f798278880599d6f70977fda38c17b
rulxx <rulibala1609@gmail.com>
2024-11-13 15:34:44 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,17 @@
+const nama = "ruli bala";
+let usia = 21;
+
+function generetBiodata() {
+    if (usia == 21) { 
+        // ini adalah kondisi pertama
+        console.log('anda dewasa');
+    } else {
+        // ini adalah jika kondisi tidak terpenuhi
+        console.log('anda anak anak');
+    }
+}
+
+console.log(nama);
+console.log(usia);
+
+generetBiodata()
